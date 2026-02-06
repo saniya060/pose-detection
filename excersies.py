38c9168e2e243e460f3ee57ad884b4618debf248
saniya060 <152259995+saniya060@users.noreply.github.com>
2026-02-06 09:33:41 +0530

    Add files via upload

---
+++ excersies.py
@@ -0,0 +1,62 @@
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.pose_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected poses to visualize.
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
+
+    # Draw the pose landmarks.
+    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      solutions.pose.POSE_CONNECTIONS,
+      solutions.drawing_styles.get_default_pose_landmarks_style())
+  return annotated_image
+# wget -q -O image.WEBP c:"Users\saniy\OneDrive\Desktop\PROJECT\exercise\tree-pose-beginner-yoga.webp"
+
+import cv2
+from image import cv2_imshow
+img = cv2.imread("image.WEBP")
+cv2_imshow(img)
+# from image import files
+# uploaded = files.upload()
+
+# for filename in uploaded:
+#   content = uploaded[filename]
+#   with open(filename, 'wb') as f:
+#     f.write(content)
+
+# if len(uploaded.keys()):
+#   IMAGE_FILE = next(iter(uploaded))
+#   print('Uploaded file:', IMAGE_FILE)
+# STEP 1: Import the necessary modules.
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+# STEP 2: Create an PoseLandmarker object.
+base_options = python.BaseOptions(model_asset_path='pose_landmarker.task')
+options = vision.PoseLandmarkerOptions(
+    base_options=base_options,
+    output_segmentation_masks=True)
+detector = vision.PoseLandmarker.create_from_options(options)
+
+# STEP 3: Load the input image.
+image = mp.Image.create_from_file("image.WEBP")
+
+# STEP 4: Detect pose landmarks from the input image.
+detection_result = detector.detect(image)
+
+# STEP 5: Process the detection result. In this case, visualize it.
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+cv2_imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
